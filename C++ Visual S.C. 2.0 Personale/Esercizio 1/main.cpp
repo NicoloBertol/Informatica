3bdb7ade21f3925b11d1770f2c16af4c4d40881d
s-nicolo.bertol@istitutopilati.it <s-nicolo.bertol@istitutopilati.it>
2025-10-16 14:48:22 +0200

    test

--- C++ Visual S.C. 2.0 Personale/Esercizio 1/main.cpp
+++ C++ Visual S.C. 2.0 Personale/Esercizio 1/main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 
+using namespace std;
+
 class Counter{
     private:
         int value;
 
     public:
-        Counter(){ }
-        ~Counter(){ }
-        int getValue(){ }
-        void increase(){ }
+        Counter(){
+            cout << "istanza dell'oggetto creata!\n";
+            value = 0;
+        }
+        ~Counter(){
+            cout << "Oggetto distrutto!\n";
+        }
+        int getValue(){
+            return value;
+        }
+        void increase(int n){
+            value+=n;
+        }
         
     
-}
-
-//using namespace std
+};
 
 int main(){
 
-    
+    Counter c1,c2;
+   
+    cout << "Contatore 1: "<< c1.getValue() <<"\n";
+    cout << "Contatore 2: "<< c2.getValue() <<"\n";
+
+    c1.increase(7);
+    c2.increase(10);
+
+    cout << "Contatore 1: "<< c1.getValue() <<"\n";
+    cout << "Contatore 2: "<< c2.getValue() <<"\n";
 
     return 0;
 }
