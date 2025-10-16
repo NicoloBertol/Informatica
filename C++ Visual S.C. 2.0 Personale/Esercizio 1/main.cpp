215f0d39a6c1b672356838fb0d89dd1b335194b1
s-nicolo.bertol@istitutopilati.it <s-nicolo.bertol@istitutopilati.it>
2025-10-16 15:01:24 +0200

    test

--- C++ Visual S.C. 2.0 Personale/Esercizio 1/main.cpp
+++ C++ Visual S.C. 2.0 Personale/Esercizio 1/main.cpp
@@ -31,11 +31,12 @@ int main(){
     cout << "Contatore 1: "<< c1.getValue() <<"\n";
     cout << "Contatore 2: "<< c2.getValue() <<"\n";
 
-    c1.increase(7);
-    c2.increase(10);
-
-    cout << "Contatore 1: "<< c1.getValue() <<"\n";
-    cout << "Contatore 2: "<< c2.getValue() <<"\n";
+    for(int i=0;i<100;i++){
+        c1.increase(1);
+        cout << "Contatore 1: "<< c1.getValue() <<"\n";
+    }
+    
+    
 
     return 0;
 }
