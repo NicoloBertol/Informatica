728dbc0872bab11add3ddacbddb3679042c0e19a
s-nicolo.bertol@istitutopilati.it <s-nicolo.bertol@istitutopilati.it>
2025-10-20 11:56:20 +0200

    init

--- C++ Visual S.C. 2.0 Personale/Esercizio 2/main.cpp
+++ C++ Visual S.C. 2.0 Personale/Esercizio 2/main.cpp
@@ -1,8 +1,19 @@
+#include <iostream>
 #include "Counter.h"
+
+using namespace std;
 
 int main(){
 
-    Counter c;
+    Counter c1,c2;
+   
+    cout << "Contatore 1: "<< c1.getValue() <<"\n";
+    cout << "Contatore 2: "<< c2.getValue() <<"\n";
+
+    for(int i=0;i<100;i++){
+        c1.increase(1);
+        cout << "Contatore 1: "<< c1.getValue() <<"\n";
+    }
 
     return 0;
 }
